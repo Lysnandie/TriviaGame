ad796c42e879b740f1c51c6b8a4685d97a727cee
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-10-18 01:34:46 -0500

    newest update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,30 @@
 //alert("i'm ready");
+$(document).ready(function(){
 
 
-//variables for anwer choices
+//variables for answers
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
-//reset game to start back at button screen
+//hidden DIVs at start of game
+$("#quiz").hide();
+$("#results").hide();
 
-//start game function? start game is false, when you click start questons start
-function startGame () {
+//start game function, when you click start questons and time starts
 
-};
+$("#startButton").on("click", function(){
+  $("#startPage").hide();
+  $("#quiz").show();
+  run();
+});
+
+
+
 
 //timer for the game (in seconds)
 var time = 120;
 var starTime;
-//To start and stop timer
-$("#startButton").on("click", run);
-$("#submitButton").on("click", stop);
 
 //This decreases timer in 1 second intervals
 function run() {
@@ -27,7 +36,10 @@ function decrement() {
 
   $("#timer").html("<h2>" + time + "</h2>");
 
+//When time hits 0, timer wills top and results will show
   if (time === 0){
+    $("#results").show();
+    $("#quiz").hide();
     stop();
   }
 }
@@ -36,14 +48,30 @@ function stop() {
   clearInterval(startTime);
 }
 
-run();
-//answer selections
+
+//Quiz Answers
+function endGame(){
+  var question1 = $("input[type='radio'][name='question1']:checked").val();
+
+  if (question1 === "Deborah Cox") {
+  correct++;
+}
+}
+console.log(endGame);
 
 //submit quiz button
+$("#submitButton").on("click", function(){
+  $("#quiz").hide();
+  $("#results").show();
 
-//need a end funciton for game (when timer is 0 game ends)
+  stop();
+  endGame();
+});
+
 
 //need to calculate total number of correct and incorrect answers at the end of game
 //.text or html (results)
-$(document).ready(function (){
+
+$("#results").text(correct);
+
 });
